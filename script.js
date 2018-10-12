6f26f6145ce85b493cef09578eb8a5bab39f39fd
Jacob Bennett <kf7ags@gmail.com>
2018-10-12 11:07:02 -0600

    calendar is now current month accurate

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+let theDate = new Date();
+let thisMonth = theDate.getMonth();
+let thisYear = theDate.getFullYear();
+let firstOfMonth = new Date(`${thisMonth + 1}/1/${thisYear}`);
+let lastOfMonth = new Date(`${thisMonth + 2}/1/${thisYear}`);
+console.log(firstOfMonth);
+let firstDay = firstOfMonth.getDay();
+let lastDay = lastOfMonth.getDay();
+console.log(`${firstDay}, ${lastDay-1}`);
+function fillContent(theID, theContent){
+    return document.getElementById(theID).innerHTML = theContent;
+}
 
 function myFunc(childNumb, element) {
-    console.log(element);
-    console.log(document.querySelectorAll("td"));
+    // console.log(element);
+    // console.log(document.querySelectorAll("td"));
     let myVar = document.querySelectorAll("td");
-    console.log(myVar);
+    // console.log(myVar);
     let myVarL = myVar.length;
-    console.log(myVarL);
+    // console.log(myVarL);
     for (let i = 0; i < myVarL; i++){
         myVar[i].setAttribute("class", "");
     }
@@ -34,3 +46,55 @@ function myFunc(childNumb, element) {
         document.getElementById("largeDayName").innerHTML = "Saturday";
     }
 }
+(function (month){
+    switch(month){
+        case 0:
+            fillContent("monthHere","January");
+            break;
+        case 1:
+            fillContent("monthHere", "February");
+            break;
+        case 2:
+            fillContent("monthHere", "March");
+            break;
+        case 3:
+            fillContent("monthHere", "April");
+            break;
+        case 4:
+            fillContent("monthHere", "May");
+            break;
+        case 5:
+            fillContent("monthHere", "June");
+            break;
+        case 6:
+            fillContent("monthHere", "July");
+            break;
+        case 7:
+            fillContent("monthHere", "August");
+            break;
+        case 8:
+            fillContent("monthHere", "September");
+            break;
+        case 9:
+            fillContent("monthHere", "October");
+            break;
+        case 10:
+            fillContent("monthHere", "November");
+            break;
+        case 11:
+            fillContent("monthHere", "December");
+            break;
+        default:
+            console.log("error in month");
+    }
+    console.log(firstOfMonth);
+})(thisMonth);
+//35 td's
+function fillCal(){
+    for (let index = firstDay, indexDate = 1; index < document.getElementsByTagName("td").length-(7-lastDay);index++,indexDate++){
+        let loopDate = new Date(`${thisMonth+1}/${indexDate}/${thisYear}`);
+        document.getElementsByTagName("td")[index].innerHTML = indexDate.toString();
+        document.getElementsByTagName("td")[index].setAttribute("onclick", `myFunc(${loopDate.getDay()+1}, this)`)
+    }
+}
+fillCal();
